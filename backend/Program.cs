19ebacf1a7c9a2ffa4ba4fc0c29c10656ab99923
st64539 <petrcerv999@gmail.com>
2025-12-16 17:04:17 +0100

    Úprava Docker compose a dockerfile

--- backend/Program.cs
+++ backend/Program.cs
@@ -115,6 +115,10 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
